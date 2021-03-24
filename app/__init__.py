96b66386e8a31f909f2a2706069e506c9a561b24
unknown <agraton12@gmail.com>
2021-03-23 20:23:04 -0500

    Final comit

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from .config import Config
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'Som3$ec5etK*y'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cnneedlquvdmhq:e2854ebd6c69f303e7e55c2d0db5f2073680bf73d90a57cf0e98132f244c88b6@ec2-54-164-22-242.compute-1.amazonaws.com:5432/dc8sj47hchbrdc'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kfakoaxmcofchc:29bb9b3871d53420170240f098e34b7321e15d0de85ffec5a8bc4ce10ce0b984@ec2-54-161-239-198.compute-1.amazonaws.com:5432/davk2k4d1i5m19'
 db = SQLAlchemy(app)
 app.config.from_object(Config)
 
