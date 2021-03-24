ba0f52ff5227419756a657222185ce3cb7be47ba
unknown <agraton12@gmail.com>
2021-03-23 21:56:00 -0500

    things

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,12 @@ from .config import Config
 #Photo Storage Location
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 app.config['SECRET_KEY'] = 'Som3$ec5etK*y'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cihhusqhulpyhk:3363f5e13040908d3f104a7ad068d94d2745096a2097231f9585af964f9ec622@ec2-54-196-111-158.compute-1.amazonaws.com:5432/d7r04rhg7l8g61'
 db = SQLAlchemy(app)
-app.config.from_object(Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cihhusqhulpyhk:3363f5e13040908d3f104a7ad068d94d2745096a2097231f9585af964f9ec622@ec2-54-196-111-158.compute-1.amazonaws.com:5432/d7r04rhg7l8g61'
+
+
 
 from app import views, models
