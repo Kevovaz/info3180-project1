1679a75b3c04fb79c1f52d7c311a990994043152
unknown <agraton12@gmail.com>
2021-03-23 19:59:12 -0500

    Added Heroku Integration

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Config(object):
 	"""Base Config Object"""
 	DEBUG = False
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://proj1:password@localhost/proj1'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://cnneedlquvdmhq:e2854ebd6c69f303e7e55c2d0db5f2073680bf73d90a57cf0e98132f244c88b6@ec2-54-164-22-242.compute-1.amazonaws.com:5432/dc8sj47hchbrdc'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
 	UPLOAD_FOLDER = 'app/static/uploads/'
 class DevelopmentConfig(Config):
